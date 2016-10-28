6bb21f524aacfa05e6b0c7d99cc38d6c94194d98
System Administrator <root@HQ-MACDEV1.home>
2016-10-28 11:08:42 -0500

    moved index.php to index.html so I dont have to install php on demo prod server lol

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# test deploy script, just write a timestamp to index.php
-echo "You deployed revision $1 at $(date)" > ./index.php
+# test deploy script, just write a timestamp to index.html
+echo "You deployed revision $1 at $(date)" > ./index.html
